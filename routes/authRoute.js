77da35b8c38701efde6a64fd24a4198a7a1350a0
fahmyfauzi <fahmyfauzii@gmail.com>
2023-10-30 23:14:03 +0700

    encrypt password with cryptoJs

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/userModel");
+const cryptoJs = require("crypto-js");
 
+//register user
 router.post("/register", async (req, res) => {
+  //create user
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: cryptoJs.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_PASS
+    ).toString(),
   });
 
   try {
